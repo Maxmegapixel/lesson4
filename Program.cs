2d4084d0d2822d1db560661268a02733bfd00834
Maksim <maxmegapixel@mail.ru>
2023-08-01 15:00:23 +0300

    задача 1 мое решение2

--- Program.cs
+++ Program.cs
@@ -1,14 +1,16 @@
 ﻿// Сумма чисел от 1 го до А
 
- static void Sum(int Num)
+ static int SumMet(int Num)
 {
     int result=0;
     for (int i=1;i<=Num;i++)
     {
         result += i;
     }
-    Console.WriteLine(result);
+    Console.WriteLine($"Сумма равна: {result}");
+    return result;
+    
 }
 Console.WriteLine("Введите число");
 int First = Convert.ToInt32(Console.ReadLine()!);
-Sum(First);
+SumMet(First);
