a02dbfcc7afa29e63ecb40ae9499f19fd6ad7ce9
Maksim <maxmegapixel@mail.ru>
2023-08-01 18:22:57 +0300

    задача 3 счастливое общее+м

--- Program.cs
+++ Program.cs
@@ -1,28 +1,25 @@
 ﻿// счастливое шестизначное число (сумма 1вых 3х цифр равна последним)
 
-static void HappyNum(string Number)
+bool HappyNum(string Number)
 {
-    int count = Number.Length;
-    Console.WriteLine($"Длина {count}");
-    if (count != 6) 
-    {
-        Console.WriteLine($"Число {Number} не шестизначное");
-    }
-    else
-    {
         int sum1 = 0;
         int sum2 = 0;
-        for (int i = 0; i < count/2; i++)
+        for (int i = 0; i < 3; i++)
         {
             sum1 += Number[i];
             sum2 += Number[i+3];
         }
-        if (sum1 == sum2) Console.WriteLine($"Число {Number} счатливое");
-        else Console.WriteLine($"Число {Number} не счатливое");
-    }
-   
+        if (sum1 == sum2) return true;
+        else return false;
 }
 
 Console.WriteLine("Введите число: ");
-string Num1 = Console.ReadLine()!;
-HappyNum(Num1);
+string Number = Console.ReadLine()!;
+int count = Number.Length;
+    Console.WriteLine($"Длина {count}");
+    if (count != 6) Console.WriteLine($"Число {Number} не шестизначное");
+    else 
+    {
+        if (HappyNum(Number) == true) Console.WriteLine($"Число {Number} счатливое");
+        else Console.WriteLine($"Число {Number} не счатливое"); 
+    }
