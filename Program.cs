58530f33c16eceff21f5b1d72b0bb792955cbcfc
Maksim <maxmegapixel@mail.ru>
2023-08-01 14:23:39 +0300

    задача 1 мое решение

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Сумма чисел от 1 го до А
+
+ static void Sum(int Num)
+{
+    int result=0;
+    for (int i=1;i<=Num;i++)
+    {
+        result += i;
+    }
+    Console.WriteLine(result);
+}
+Console.WriteLine("Введите число");
+int First = Convert.ToInt32(Console.ReadLine()!);
+Sum(First);
