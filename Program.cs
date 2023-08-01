9346a6eff59174608c150eb91a162dfb85dc368d
Maksim <maxmegapixel@mail.ru>
2023-08-01 17:25:43 +0300

    задача 2 произведение

--- Program.cs
+++ Program.cs
@@ -1,15 +1,15 @@
-﻿// Сумма чисел от 1 го до А
+﻿// Произведение чисел от 1 го до А
 
- static int SumMet(int Num)
+ static int ProdGet(int Num)
 {
-    int result=0;
+    int result=1;
     for (int i=1;i<=Num;i++)
     {
-        result += i;
+        result *= i;
     }
     return result;
 }
 Console.WriteLine("Введите число");
 int First = Convert.ToInt32(Console.ReadLine()!);
-int Sum = SumMet(First);
+int Sum = ProdGet(First);
 Console.WriteLine($"Сумма равна: {Sum}");
